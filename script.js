07d95f34b9d32690ff4370a95165d1c918ce529c
Godizera <godipalhares@gmail.com>
2025-11-18 19:54:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+// Botão de tema claro/escuro
+const temaBtn = document.getElementById("temaBtn");
+temaBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
+// FORMULÁRIO - VALIDAÇÃO
+document.getElementById("formContato").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let nome = document.getElementById("nome").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let msg = document.getElementById("mensagem").value.trim();
+    let status = document.getElementById("status");
+
+    // Verificar campos vazios
+    if (nome === "" || email === "" || msg === "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    // Validação simples de e-mail
+    if (!email.includes("@") || !email.includes(".")) {
+        alert("Digite um e-mail válido!");
+        return;
+    }
+
+    // Simulação de envio
+    alert("Mensagem enviada com sucesso!");
+
+    // Limpar formulário
+    document.getElementById("formContato").reset();
+});
